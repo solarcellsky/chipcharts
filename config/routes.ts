6c6a5d20cfba690f3d6c8c76a3cbaa33e265fe7c
solar <solarcellsky@gmail.com>
2021-09-08 10:23:37 +0800

    ci: 🎡 init code

---
+++ config/routes.ts
@@ -0,0 +1,9 @@
+export default [
+  {
+    exact: true,
+    name: '首页',
+    title: 'Dashboard',
+    path: '/',
+    component: '@/pages/dashboard/index',
+  },
+];
---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Layout, Button } from 'antd';
+import {
+  CodeOutlined,
+  SaveOutlined,
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+} from '@ant-design/icons';
+import DemoScatter from './components/charts';
+import DemoRadar from './components/radar';
+import logo from '/public/assets/logo.png';
+import styles from './index.less';
+const mountNode = document.getElementById('root');
+
+class DashBoard extends React.Component {
+  state = {
+    size: 'small',
+    collapsed: false,
+  };
+
+  handleSideCollapse = () => {
+    this.setState({ collapsed: !this.state.collapsed });
+  };
+
+  render() {
+    const { Header } = Layout;
+    const { size, collapsed } = this.state;
+    return (
+      <>
+        <Layout>
+          <Header className={styles.pageheader}>
+            <img src={logo} className={styles.logo} alt="" />
+          </Header>
+          <Layout className={styles.mainContent}>
+            <div
+              className={
+                collapsed ? styles.cols + ' ' + styles.collapsed : styles.cols
+              }
+            >
+              <div className={styles.innerWrapper}>
+                <h5>Dataset</h5>
+                <Button
+                  size={size}
+                  icon={
+                    collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />
+                  }
+                  className={styles.colBtn}
+                  onClick={this.handleSideCollapse}
+                />
+              </div>
+            </div>
+            <div className={styles.cols}>
+              <div className={styles.innerWrapper}>
+                <div>
+                  <Button size={size} icon={<CodeOutlined />}>
+                    RUN
+                  </Button>
+                  <Button size={size} icon={<SaveOutlined />}>
+                    SAVE
+                  </Button>
+                </div>
+                <DemoScatter />
+                <DemoRadar />
+              </div>
+            </div>
+            <div
+              className={
+                collapsed ? styles.cols + ' ' + styles.collapsed : styles.cols
+              }
+            >
+              <div className={styles.innerWrapper}>
+                <h5>Result</h5>
+              </div>
+            </div>
+          </Layout>
+        </Layout>
+      </>
+    );
+  }
+}
+
+ReactDOM.render(<DashBoard />, mountNode);
